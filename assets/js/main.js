53e1cef10fe0ca67b09a37fa6ce61d0823a04369
Barnali Kundu <bk.tech204@gmail.com>
2024-10-01 19:14:24 +0530

    adding assets

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,10 +35,45 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Function to add the 'visible' class to all sections after the page has loaded
   window.addEventListener('load', () => {
-    const sections = document.querySelectorAll('.section'); 
+    const sections = document.querySelectorAll('.sections'); 
     sections.forEach((section, index) => {
       setTimeout(() => {
         section.classList.add('visible'); 
       }, index * 200);
     });
+  });
+
+
+
+  // Global section  transitions
+  const sec = document.querySelectorAll('.section');
+  const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('visible');
+              observer.unobserve(entry.target); 
+          }
+      });
+      }, {
+      threshold: 0.5 
+      });
+
+      sec.forEach(sections => {
+      observer.observe(sections);
+  });
+
+  const smallsec = document.querySelectorAll('.small-section');
+  const observe = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('visible');
+              observer.unobserve(entry.target); 
+          }
+      });
+      }, {
+      threshold: 0.5 
+      });
+
+      smallsec.forEach(smallsections => {
+      observe.observe(smallsections);
   });
